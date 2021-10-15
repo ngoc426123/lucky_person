fbed101ba2c4b0f43f6f408e9358a47893292e63
MinhNgoc <minhngoc.ith@gmail.com>
2021-10-15 15:14:22 +0700

    15:15 15/10/2021: complete.

--- src/js/plugins/lucky-person.js
+++ src/js/plugins/lucky-person.js
@@ -1,7 +1,13 @@
 @Plugin({
   options: {
+    personHeight: 200,
+    numberAnonymous: 3,
+    dataBoxlucky: '[data-box-lucky]',
     dataListPerson: '[data-list-person]',
     dataBtnSpin: '[data-btn-spin]',
+    clsSpining: 'spinning',
+    clsBingo: 'bingo',
+    clsNotClick: 'not-click',
   },
   
 })
@@ -12,7 +18,12 @@ export default class LuckyPerson {
       dataRender: [],
       name: 'ngoc',
       count: 0,
+      offsetMin: 0,
       offsetMax: 0,
+      offsetClone: 0,
+      spin: null,
+      isSpin: false,
+      isPreventFastSpin: false,
     };
     this.initDOM();
     this.handleEvent();
@@ -21,10 +32,12 @@ export default class LuckyPerson {
   initDOM () {
     const { $element } = this;
     const {
+      dataBoxlucky,
       dataListPerson,
       dataBtnSpin
     } = this.options;
 
+    this.$boxLucky = $element.find(dataBoxlucky);
     this.$listPerson = $element.find(dataListPerson);
     this.$btnSpin = $element.find(dataBtnSpin);
   }
@@ -46,6 +59,7 @@ export default class LuckyPerson {
   }
 
   initGame (data) {
+    const { personHeight, numberAnonymous } = this.options;
     const DataClone = () => {
       const numberClone = () => data.count < 10 ? 5 : 10;
       const cloneUpData = data.slice(-numberClone()).map(person => { return { ...person, clone: true } });
@@ -53,7 +67,8 @@ export default class LuckyPerson {
       const cloneDownData = data.slice(0, numberClone()).map(person => { return { ...person, clone: true } });
       const dataMerge = [...cloneUpData, ...mainData, ...cloneDownData];
       const dataOffset = dataMerge.map((person, index) => {
-        const offset = -(index * 250 + 625)
+        const offset = - (index * personHeight + (personHeight * numberAnonymous - (personHeight / 2)));
+
         return {
           ...person,
           offset,
@@ -66,12 +81,51 @@ export default class LuckyPerson {
     this.props.data = data;
     this.props.dataRender = DataClone();
     this.props.count = this.props.data.length;
-    this.props.offsetMax = this.props.data.reduce((curr, acc) =>  curr -= 250, -375);
+    this.props.offsetMin = this.props.dataRender[1].offset;
+    this.props.offsetMax = this.props.dataRender[this.props.dataRender.length - 2].offset;
+    this.props.offsetClone = (data.count < 10 ? 5 : 10) * personHeight;
+    this.props.spin = gsap.timeline({ defaults: {duration: 0, ease: 'none'}});
+    console.log(this.props);
     this.renderPerson();
   }
 
-  handleSpin () {
-    console.log(this.randomPerson());
+  async handleSpin () {
+    const { spin, isSpin, isPreventFastSpin } = this.props;
+    const { clsSpining, clsBingo } = this.options;
+
+    if ( isPreventFastSpin ) {
+      return;
+    }
+
+    if ( !isSpin ) {
+      this.props.isSpin = true;
+      this.props.isPreventFastSpin = true;
+      this.toggleSpin();
+      this.$listPerson.addClass(clsSpining);
+      spin.play();
+      spin.fromTo(this.$listPerson, { y: this.props.offsetMin }, { y: this.props.offsetMax, repeat: -1, duration: 0.5, ease: 'none' });
+      await new Promise((reslove, reject) => {setTimeout(reslove, 1500)});
+      this.props.isPreventFastSpin = false;
+      this.toggleSpin();
+    } else {
+      const randomPerson = this.randomPerson();
+      const positionPerson = this.getPositionPerson(randomPerson);
+      const positionEdge = positionPerson - this.props.offsetClone;
+
+      this.props.isSpin = false;
+      this.props.isPreventFastSpin = true;
+      this.toggleSpin();
+      this.$listPerson.removeClass(clsSpining);
+      spin.pause();
+      gsap.fromTo(this.$listPerson, { y: positionEdge }, { y: positionPerson, duration: 4, ease: 'circ.out', onComplete: () => {
+        this.$boxLucky.addClass(clsBingo);
+        this.$boxLucky.on('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
+          this.$boxLucky.removeClass(clsBingo);
+          this.props.isPreventFastSpin = false;
+          this.toggleSpin();
+        })
+      }});
+    }
   }
 
   renderPerson () {
@@ -103,11 +157,30 @@ export default class LuckyPerson {
 
   randomPerson () {
     const { data } = this.props;
-    const randomIndex = Math.floor(Math.random() * data.length - 1) + 0;
+    const randomIndex = parseInt(Math.floor(Math.random() * (data.length - 1)) + 0);
     const randomPerson = data[randomIndex];
 
-    this.props.data = data.splice(randomIndex, 1);
-    console.log(data.splice(randomIndex, 1));
+    this.props.data.splice(randomIndex, 1);
     return randomPerson;
+  }
+
+  getPositionPerson (personRandom) {
+    const { dataRender } = this.props;
+    const { name } = personRandom;
+    const person = dataRender.filter(person => person.clone === false && person.name === name);
+    const offset = person[0].offset;
+
+    return offset;
+  }
+
+  toggleSpin () {
+    const { clsNotClick, clsSpining } = this.options;
+    const { isSpin, isPreventFastSpin } = this.props;
+
+    isPreventFastSpin && this.$btnSpin.addClass(clsNotClick);
+    !isPreventFastSpin && this.$btnSpin.removeClass(clsNotClick);
+
+    isSpin && this.$btnSpin.addClass(clsSpining);
+    !isSpin && this.$btnSpin.removeClass(clsSpining);
   }
 }
